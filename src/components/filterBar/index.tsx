1ca1c1af69e71e48e2cb0e424bd7e3e0eb2d912b
Murad <mur1d1xmedov@gmail.com>
2023-11-17 10:48:18 +0400

    useFormik and yup added to filter form

--- src/components/filterBar/index.tsx
+++ src/components/filterBar/index.tsx
@@ -1,18 +1,52 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import styles from "./assets/css/styles.module.css";
 
 function FilterBar() {
+  const initialValues = {
+    min: "",
+    max: "",
+    rooms: "",
+    floor: "",
+  };
+
+  const validationSchema = Yup.object({
+    min: Yup.number(),
+    max: Yup.number(),
+    rooms: Yup.number(),
+    floor: Yup.number(),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
+
   return (
     <>
       <div className={`wrapper ${styles.wrapper}`}>
         <div className={`container ${styles.container}`}>
-          <form className={`my-5 p-3 px-4 px-sm-3 ${styles.form}`}>
+          <form
+            onSubmit={formik.handleSubmit}
+            className={`my-5 p-3 px-4 px-sm-3 ${styles.form}`}
+          >
             <div className="row align-items-center justify-content-around row-gap-2 mb-2">
               <input
+                name="min"
+                value={formik.values.min}
+                onChange={formik.handleChange}
                 className={`col col-12 col-sm-5 ${styles.input}`}
                 type="text"
                 placeholder="Price Min."
               />
               <input
+                name="max"
+                value={formik.values.max}
+                onChange={formik.handleChange}
                 className={`col col-12 col-sm-5 ${styles.input}`}
                 type="text"
                 placeholder="Price Max."
@@ -20,11 +54,17 @@ function FilterBar() {
             </div>
             <div className="row align-items-center justify-content-around row-gap-2 mb-2">
               <input
+                name="rooms"
+                value={formik.values.rooms}
+                onChange={formik.handleChange}
                 className={`col col-12 col-sm-5 ${styles.input}`}
                 type="text"
                 placeholder="Rooms"
               />
               <input
+                name="floor"
+                value={formik.values.floor}
+                onChange={formik.handleChange}
                 className={`col col-12 col-sm-5 ${styles.input}`}
                 type="text"
                 placeholder="Floor"
