4587a0a9ba4a2f10d576e9fd6bdb08ff285ceeaa
Murad <mur1d1xmedov@gmail.com>
2023-11-17 09:30:09 +0400

    house details modal prepared

--- src/components/houseCard/index.tsx
+++ src/components/houseCard/index.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import IHouse from "../../models/IHouse";
 import styles from "./assets/css/styles.module.css";
+import HouseDetailsModal from "../houseDetailModal";
 
 interface HouseCardProps {
   house: IHouse;
 }
 
 const HouseCard: React.FC<HouseCardProps> = ({ house }) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const openModal = () => {
+    setIsOpenModal(true);
+    document.body.style.overflow = "hidden"; // body'ye overflow: hidden; eklenir
+  };
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+    document.body.style.overflow = ""; // body'den overflow stilini kaldırır
+  };
+
   return (
     <>
-      <div className={styles.card}>
+      <div onClick={openModal} className={styles.card}>
         <div className={styles.coverImage}>
           <img src={house.images[0]} alt="cover image" />
         </div>
@@ -20,7 +33,7 @@ const HouseCard: React.FC<HouseCardProps> = ({ house }) => {
             {house.location}
           </p>
           <div className="row align-items-center justify-content-between">
-            <div className={`col col-4 text-center ${styles.detail}`}>
+            <div className={`col col-4 ${styles.detail}`}>
               <i className="bi bi-buildings" />
               {house.floor}
             </div>
@@ -28,13 +41,17 @@ const HouseCard: React.FC<HouseCardProps> = ({ house }) => {
               <i className="bi bi-door-open" />
               {house.rooms}
             </div>
-            <div className={`col col-4 text-center ${styles.detail}`}>
+            <div className={`col col-4 ${styles.detail}`}>
               <i className="bi bi-x-diamond" />
               {house.area}
             </div>
           </div>
         </div>
       </div>
+
+      {isOpenModal && (
+        <HouseDetailsModal closeModal={closeModal} house={house} />
+      )}
     </>
   );
 };
--- src/components/houseDetailModal/index.tsx
+++ src/components/houseDetailModal/index.tsx
@@ -1,5 +1,57 @@
-function HouseDetailsModal() {
-  return <div>HouseDetailsModal</div>;
+import React from "react";
+import IHouse from "../../models/IHouse";
+import styles from "./assets/css/styles.module.css";
+
+interface HouseDetailsModalProps {
+  house: IHouse;
+  closeModal: () => void;
 }
+
+const HouseDetailsModal: React.FC<HouseDetailsModalProps> = ({
+  house,
+  closeModal,
+}) => {
+  return (
+    <>
+      <div className={` ${styles.wrapper}`}>
+        <div className="container">
+          <div className={styles.houseDetails}>
+            <div
+              className={`d-flex align-items-center justify-content-between ${styles.header}`}
+            >
+              <h4 className={`col col-11 ${styles.title}`}>
+                {house.rooms}-otaq Yeni Mənzil {house.area}m²
+                <br /> {house.location}
+              </h4>
+              <i
+                onClick={closeModal}
+                className="bi bi-x-circle text-center col col-1"
+              />
+            </div>
+            <div className={styles.gallery}>
+              {house.images.map((image, index) => (
+                <img key={index} src={image} alt="image" />
+              ))}
+            </div>
+            <div className="row align-items-center justify-content-between">
+              <div className={`col col-4 ${styles.detail}`}>
+                <i className="bi bi-buildings" />
+                {house.floor}
+              </div>
+              <div className={`col col-4 text-center ${styles.detail}`}>
+                <i className="bi bi-door-open" />
+                {house.rooms} rooms
+              </div>
+              <div className={`col col-4 ${styles.detail}`}>
+                <i className="bi bi-x-diamond" />
+                {house.area}m²
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HouseDetailsModal;
